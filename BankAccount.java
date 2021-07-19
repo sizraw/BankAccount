bd88881ef8f0fe3f1237a7ddd94e9f60b807035c
Rajeeb Manandhar <rajeebmanandhar@Rajeebs-MacBook-Pro-2.local>
2021-07-18 17:59:51 -0700

    first commit

--- BankAccount.java
+++ BankAccount.java
@@ -7,6 +7,13 @@ public class BankAccount {
     private String customerName;
     private long phoneNumber;
 
+    BankAccount(String customerName, int accountnumber, float balance, long phonenumber, String Email){
+        this.accountnumber = accountnumber;
+        this.customerName = customerName;
+        this.balance = balance;
+        this.phoneNumber = phonenumber;
+        this.email = Email;
+    }
     public String getEmail() {
         return email;
     }
@@ -49,47 +56,5 @@ public class BankAccount {
         this.phoneNumber = phoneNumber;
     }
 
-    public void method(String Name) {
-        if (Name.equals("b")) {
-            System.out.println("Here is the information of the customer!!");
-            System.out.println("***************************");
-            System.out.println("Name: " + this.getCustomerName());
-            System.out.println("Account Number: " + this.getAccountnumber());
-            System.out.println("Email: " + this.getEmail());
-            System.out.println("Phone Number: " + this.getPhoneNumber());
-            System.out.println("Account Balance: " + this.balance);
-        } else if (Name.equals("d")) {
-            System.out.println("Please enter the amount to deposit: ");
-            Scanner input = new Scanner(System.in);
-            float str = input.nextFloat();
-            this.balance = this.balance + str;
-            System.out.println("Your new balance is " + this.balance);
-        } else if (Name.equals("w")) {
-            System.out.println("Please enter the amount to withdraw: ");
-            Scanner input = new Scanner(System.in);
-            float str = input.nextFloat();
 
-            if (str <= this.balance) {
-                this.balance = this.balance - str;
-                System.out.println("Your new balance is " + this.balance);
-            } else {
-                System.out.println("Sorry you do not have enough balance to withdraw");
-            }
-        }
-        else if(Name.equals("e")){
-            System.out.println("Bye....");
-            System.exit(0);
-        }
-    }
-    public void depositFund(float deposit) {
-        System.out.println(this.balance + deposit);
-    }
-
-    public void WithdrawFund(float withdraw) {
-        if (this.balance > withdraw) {
-            System.out.println(this.balance - withdraw);
-        } else {
-            System.out.println("not enough fund");
-        }
-    }
 }
--- Main.java
+++ Main.java
@@ -1,43 +1,121 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		BankAccount account = new BankAccount();
+		Bank account = new BankAccountDaoImpl();
 
-		System.out.println("Please enter your full name: ");
-		Scanner input = new Scanner(System.in);
-		String name = input.nextLine();//Reads string
-		account.setCustomerName(name);
 
-		System.out.println("Please enter your account number: ");
-		Scanner ainput = new Scanner(System.in);
-		int accountt = input.nextInt();//Reads string
-		account.setAccountnumber(accountt);
 
-		System.out.println("Please enter amount to deposit: ");
-		Scanner deposit = new Scanner(System.in);
-		Float depositt= deposit.nextFloat();//Reads string
-		account.setBalance(depositt);
+	do{
+		System.out.println("**************************************");
+		System.out.println("Please select the following option: ");
+		System.out.println("d for Deposit");
+		System.out.println("w for Withdraw");
+		System.out.println("b to check Balance");
+		System.out.println("e to Exit");
+		System.out.println("u to update");
+		System.out.println("a to add users");
+		System.out.println("D to delete");
+		System.out.print("p to print all users: ");
+		Scanner inputt = new Scanner(System.in);
+		char str = inputt.next().charAt(0);//Reads string
 
-		System.out.println("Please enter your email address: ");
-		Scanner emailinput = new Scanner(System.in);
-		String email= emailinput.nextLine();//Reads string
-		account.setEmail(email);
+		switch(str) {
 
-		System.out.println("Please enter your phone number: ");
-		Scanner phoneinput = new Scanner(System.in);
-		long phone= phoneinput.nextLong();//Reads string
-		account.setPhoneNumber(phone);
+			//Deposit
+			case 'd':
+				System.out.println("Please enter your Full Name: ");
+				Scanner AccountInput = new Scanner(System.in);
+				String InputAccount = AccountInput.nextLine();
+				System.out.println("Please enter amount to deposit ");
+				Scanner InputDeposit = new Scanner(System.in);
+				Float Deposit = InputDeposit.nextFloat();
+				account.Deposit(InputAccount, Deposit);
+				//account.updateUsers();
+				break;
 
-		do {
-			System.out.println("Please enter 'd' for deposit, 'w' for withdraw, 'b' for balance and 'e' for exit: ");
-			Scanner inputt = new Scanner(System.in);
-			String str = inputt.nextLine();//Reads string
-			account.method(str);
+			//Withdraw
+			case 'w':
+
+				System.out.println("Please enter your Full Name: ");
+				Scanner AccounttInput = new Scanner(System.in);
+				String InputAccountt = AccounttInput.nextLine();
+				System.out.println("Please enter amount to withdraw ");
+				Scanner InputW = new Scanner(System.in);
+				Float withdraw = InputW.nextFloat();
+				account.Withdraw(InputAccountt, withdraw);
+				break;
+
+			//check Balance
+			case 'b':
+				System.out.println("Please enter your Full Name: ");
+				Scanner input = new Scanner(System.in);
+				String accountNumber = input.nextLine();
+				account.CheckBalance(accountNumber);
+
+				break;
+
+			//exit
+			case 'e':
+				System.exit(0);
+				System.out.println("Bye");
+				break;
+
+			//update
+			case 'u':
+
+				System.out.println("Please enter your Full Name or Email or Phone Number: ");
+				Scanner inputtt = new Scanner(System.in);
+				String number = inputtt.nextLine();
+				System.out.println("Please enter the update one: ");
+				Scanner Inputtt = new Scanner(System.in);
+				String newName = Inputtt.nextLine();
+				account.updateUsers(number, newName);
+
+				break;
+
+			//delete
+			case 'D':
+				System.out.println("Please enter your Full Name: ");
+				Scanner dinput = new Scanner(System.in);
+				int NameToDelete = dinput.nextInt();
+				account.deleteUsers(account.getUsers(NameToDelete), NameToDelete);
+				break;
+
+			case 'a':
+				System.out.println("<<<<Creating new account>>>>>>");
+				System.out.println("Please enter your full name: ");
+				Scanner Newinput = new Scanner(System.in);
+				String NewName = Newinput.nextLine();
+				System.out.println("Please enter the amount to deposit: ");
+				Scanner newinput = new Scanner(System.in);
+				Float newBalance = newinput.nextFloat();
+				System.out.println("Please enter PhoneNumber: ");
+				Scanner Nnewinput = new Scanner(System.in);
+				int NewPhone = Nnewinput.nextInt();
+				System.out.println("Please enter your email address: ");
+				Scanner NNewinput = new Scanner(System.in);
+				String NewEmail =NNewinput.nextLine();
+				Random random = new Random();
+				int AccountNumber = random.nextInt();
+				BankAccount newAccount = new BankAccount(NewName, AccountNumber, newBalance, NewPhone, NewEmail);
+				account.Addaccount(newAccount);
+				break;
+
+			case 'p':
+				//printing all users
+				for (BankAccount users : account.getAllUsers()) {
+					System.out.println("Users: Name : " + users.getCustomerName() + ", AccountNumber : " + users.getAccountnumber() + ", PhoneNumber : " + users.getPhoneNumber() +  ", Email: " + users.getEmail() + ", Balance: " + users.getBalance());
+				}
+				break;
+		}
+
+
 
 		}
-		while (true);
-	}
+	while(true);
 
+	}
 }
